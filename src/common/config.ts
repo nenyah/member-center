2935ae838acdd3b52e8e44505a040f7f25322712
steven <steven@nenyah.com>
2021-01-19 16:59:40 +0800

    feat(config.ts): add indexImg

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,6 +1,7 @@
 export const appConfig = {
     tokenKey: 'Authorization',
     apiUrl:
-        process.env.NODE_ENV === 'production' ? '*' : '*',
+        process.env.NODE_ENV === 'production' ? 'https://dev.huadongbio.com:8505/' : 'https://dev.huadongbio.com:8505/',
     indexImg: 'http://192.168.0.175:8500/shanjian-app/index-cover.jpg',
+    privacyImg: 'http://192.168.0.175:8500/shanjian-app/privacy.jpg',
 }
