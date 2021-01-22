d0d55b347fcff96776d5790e17690179a9a81852
steven <steven@nenyah.com>
2021-01-22 16:55:50 +0800

    feat: change ip to domain

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,6 +2,8 @@ export const appConfig = {
     tokenKey: 'Authorization',
     apiUrl:
         process.env.NODE_ENV === 'production' ? 'https://dev.huadongbio.com:8505/' : 'https://dev.huadongbio.com:8505/',
-    indexImg: 'http://192.168.0.175:8500/shanjian-app/index-cover.jpg',
-    privacyImg: 'http://192.168.0.175:8500/shanjian-app/privacy.jpg',
+    indexImg: 'http://dev.huadongbio.com:8500/shanjian-app/index-cover.jpg',
+    privacyImg: 'http://dev.huadongbio.com:8500/shanjian-app/privacy.jpg',
+    productImg: 'http://dev.huadongbio.com:8500/shanjian-app/goods.png',
+    product: '膳见纤玺 高纤多维复合蛋白固体饮料（20g*10 包）',
 }
